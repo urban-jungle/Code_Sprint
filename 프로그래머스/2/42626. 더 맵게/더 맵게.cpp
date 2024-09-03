f7a8f0500160f1f228036aa40f8cb07be2bad1a9
이정민 <162151491+urban-jungle@users.noreply.github.com>
2024-09-04 07:44:55 +0900

    [level 2] Title: 더 맵게, Time: 221.25 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    while(pq.size() > 1 && pq.top() < K){
+        int first = pq.top();
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
+        
+        int n = first + (second * 2);
+        pq.push(n);
+        answer++;
+    }
+    if(pq.top() < K){
+        return -1;
+    }
+    return answer;
+}
