ee85a06936f25412964e89ad9168286326db87d7
이정민 <162151491+urban-jungle@users.noreply.github.com>
2025-01-09 03:15:07 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 2.93 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << to_string(a) << " + " << to_string(b) << " = " << a + b << endl;
+    return 0;
+}
