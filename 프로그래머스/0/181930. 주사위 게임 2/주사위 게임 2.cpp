0d001a994dac07666854a4c68043af9ab85b81e5
이정민 <162151491+urban-jungle@users.noreply.github.com>
2025-01-11 02:52:13 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    if((a != b) && (b != c) && (a != c)){
+        answer += a + b + c;
+    }
+    else if(((a == b) && (b != c)) || ((b == c) && (a != c)) || ((a == c) && (b != c))){
+        answer += (a + b + c) * (a*a + b*b + c*c);
+    }
+    else if((a == b) && (b == c)){
+        answer += (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+    }
+    return answer;
+}
