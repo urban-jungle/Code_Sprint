f93a5b7799b387624811877bd31f4e34ccbbaba9
이정민 <162151491+urban-jungle@users.noreply.github.com>
2025-01-11 03:39:06 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    if(num_list[num_list.size() - 1] > num_list[num_list.size() - 2]){
+        num_list.push_back(num_list[num_list.size() - 1] - num_list[num_list.size() - 2]);
+    }
+    else{
+        num_list.push_back(num_list[num_list.size() - 1] * 2);
+    }
+    return num_list;
+}
