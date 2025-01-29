4d00ea07eb737a677388b0382f0ea16351e54c4a
JungMin Lee <162151491+urban-jungle@users.noreply.github.com>
2025-01-29 22:17:53 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int solution(string my_string, string target) {
+    if (my_string.find(target) != string::npos) {
+        return 1;
+    }
+    return 0;
+}
