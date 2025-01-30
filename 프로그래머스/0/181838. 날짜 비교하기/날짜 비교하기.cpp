57fe1345af8afccde07e4155655b447ceef0ce9f
JungMin Lee <162151491+urban-jungle@users.noreply.github.com>
2025-01-30 15:56:40 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    if (date1[0] < date2[0]){
+        return 1;
+    }
+    else if (date1[0] == date2[0]){
+        if (date1[1] < date2[1]){
+            return 1;
+        }
+        else if (date1[1] == date2[1]){
+            if (date1[2] < date2[2]){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
