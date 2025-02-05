97028c5676c5ffe0692d3d24f6018bbb292f16cc
JungMin Lee <162151491+urban-jungle@users.noreply.github.com>
2025-02-05 23:06:09 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.22 ms, Memory: 3.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for(int i = 0; i < numbers.size(); i++){
+        numbers[i] *= 2;
+    }
+    answer = numbers;
+    return answer;
+}
