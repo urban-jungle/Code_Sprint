cf100d21e0fe3afe6da1efc8ad8532f49552812d
JungMin Lee <162151491+urban-jungle@users.noreply.github.com>
2025-02-10 17:46:47 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if(price >= 100000 && price < 300000){
+        answer += price - (price * 0.05);
+    }
+    else if(price >= 300000 && price < 500000){
+        answer += price - (price * 0.1);
+    }
+    else if(price >= 500000){
+        answer += price - (price * 0.2);
+    }
+    else{
+        answer = price;
+    }
+    return answer;
+}
